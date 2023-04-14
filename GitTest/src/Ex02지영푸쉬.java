40619176129937e202b93c2f2be4534058413ea9
smhrd <smhrd@DESKTOP-HM0JUMQ>
2023-04-14 16:52:36 +0900

    유광 커밋

--- GitTest/src/Ex02지영푸쉬.java
+++ GitTest/src/Ex02지영푸쉬.java
@@ -5,7 +5,8 @@ public class Ex02지영푸쉬 {
 		System.out.println("지영 푸쉬했음!");
 		
 		System.out.println("대혁 풀완료!");
-
+		
+		System.out.println("");
 	}
 
 }
---
+++ GitTest/src/Ex03대혁대혁.java
@@ -0,0 +1,9 @@
+
+public class Ex03대혁대혁 {
+
+	public static void main(String[] args) {
+		System.out.println("멋진 대혁이형");
+
+	}
+
+}
