da7f6ec84ee14e73dc3db0f325c52ab9356671bd
SMHRD <SMHRD@DESKTOP-BK5155A>
2023-04-14 17:15:45 +0900

    지영 클래스 생성

---
+++ GitTestJTeam/src/Jiyoung.java
@@ -0,0 +1,19 @@
+
+public class Jiyoung {
+
+	public static void main(String[] args) {
+		System.out.println("지영이꺼");
+		System.out.println("지영이꺼");
+		System.out.println("지영이꺼");
+		System.out.println("지영이꺼");
+		System.out.println("지영이꺼");
+		System.out.println("지영이꺼");
+		System.out.println("지영이꺼");
+		System.out.println("지영이꺼");
+		System.out.println("지영이꺼");
+		
+		
+
+	}
+
+}
